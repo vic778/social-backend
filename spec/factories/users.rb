7c80c681de51e2ebb72e70e7934c6eedc4253b2b
vic778 <victoiremmanuelbarh@gmail.com>
2022-05-31 18:44:44 +0300

    Generate a fake user to test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :user do
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    name { Faker::Name.name }
+    profile { Faker::Lorem.paragraph }
+    bio { Faker::Lorem.paragraph }
   end
 end

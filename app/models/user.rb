407e5b4695c779f886d097612153b71f068a0105
vic778 <victoiremmanuelbarh@gmail.com>
2022-05-31 18:40:56 +0300

    Generate test

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+   def generate_jwt
+    JWT.encode({ id: id,exp: 30.days.from_now.to_i },'juubixSecret')
+  end
+end
---
+++ app/views/users/_user.json.jbuilder
@@ -0,0 +1,2 @@
+json.(user,:id,:email,:name,:profile,:bio)
+json.token user.generate_jwt

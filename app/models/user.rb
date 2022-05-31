05faed51c17dc1b6d5105ee175261adb8bcb07fc
vic778 <victoiremmanuelbarh@gmail.com>
2022-05-31 23:57:23 +0300

    Associate post to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  mount_uploader :profile, ImageUploader
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+
+  has_many :posts
 
   def generate_jwt
     JWT.encode({ id: id, exp: 30.days.from_now.to_i }, 'vicSecret')

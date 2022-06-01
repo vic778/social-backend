27a042766b562433c18b4e27eafb4036bb209fbe
vic778 <victoiremmanuelbarh@gmail.com>
2022-06-01 10:51:50 +0300

    Fix linter errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,29 +1,28 @@
 class CommentsController < PermissionsController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def create
-        @comment = Comment.create(comment_params.merge(user_id: current_user.id))
-        @comment.post_id = params[:post_id]
-        if @comment.save
-            render json: { success: true, message: "Comment created successfully", comment: @comment }, status: :created
-        else
-            render json: { success: false, message: @comment.errors.full_messages }, status: 422
-        end
+  def create
+    @comment = Comment.create(comment_params.merge(user_id: current_user.id))
+    @comment.post_id = params[:post_id]
+    if @comment.save
+      render json: { success: true, message: "Comment created successfully", comment: @comment }, status: :created
+    else
+      render json: { success: false, message: @comment.errors.full_messages }, status: 422
     end
+  end
 
-    def destroy
-        @comment = Comment.find(params[:id])
-        if @comment.destroy
-            render json: { success: true, message: "Comment deleted successfully", comment: @comment }, status: :ok
-        else
-            render json: { success: false, message: @comment.errors.full_messages, comment: @comment }, status: 422
-        end
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      render json: { success: true, message: "Comment deleted successfully", comment: @comment }, status: :ok
+    else
+      render json: { success: false, message: @comment.errors.full_messages, comment: @comment }, status: 422
     end
+  end
 
-    private
+  private
 
-    def comment_params
-        params.permit(:post_id, :user_id, :content)
-    end
-
+  def comment_params
+    params.permit(:post_id, :user_id, :content)
+  end
 end

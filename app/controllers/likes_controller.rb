be90cf84c81f1a3c736b02dd50f90769b277c83d
vic778 <victoiremmanuelbarh@gmail.com>
2022-06-01 11:28:12 +0300

    Fix linter error

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,17 @@
 class LikesController < PermissionsController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def create
-        @post = Post.find(params[:post_id])
-        @like = @post.likes.build(user_id: current_user.id)
-        @like.save
-        render json: { post: @post, like: @like }
-    end
+  def create
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.build(user_id: current_user.id)
+    @like.save
+    render json: { post: @post, like: @like }
+  end
 
-    def destroy
-        @post = Post.find(params[:post_id])
-        @like = @post.likes.find_by(user_id: current_user.id)
-        @like.destroy
-        render json: { post: @post, like: @like }
-    end
-
+  def destroy
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.find_by(user_id: current_user.id)
+    @like.destroy
+    render json: { post: @post, like: @like }
+  end
 end

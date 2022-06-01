9b6a0fb92564419f6be4b82bc310895c6227480e
vic778 <victoiremmanuelbarh@gmail.com>
2022-06-01 11:26:38 +0300

    Add routes for likes

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,4 +2,5 @@ class PostSerializer < ActiveModel::Serializer
   attributes :id, :image, :description, :video, :user_id, :created_at, :updated_at
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :profile, :bio, :created_at, :updated_at
   has_many :posts
   has_many :comments
+  has_many :likes
 end

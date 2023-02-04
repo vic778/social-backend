7e3d4b48284cfade536b0f53076d5832a125986c
vic778 <victoiremmanuelbarh@gmail.com>
2023-02-04 23:33:04 +0400

    Create ppost and delete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,21 +2,23 @@ class CommentsController < PermissionsController
   before_action :authenticate_user!
 
   def create
-    @comment = Comment.create(comment_params.merge(user_id: current_user.id))
-    @comment.post_id = params[:post_id]
-    if @comment.save
-      render json: { success: true, message: "Comment created successfully", comment: @comment }, status: :created
+    post = Post.find_by(id: params[:post_id])
+    comment = post.comments.build(comment_params)
+    comment.user_id = current_user.id
+    if comment.save
+      render json: { success: true, message: "Comment created successfully", comment: comment }, status: :created
     else
-      render json: { success: false, message: @comment.errors.full_messages }, status: 422
+      render json: { success: false, message: comment.errors.full_messages }, status: 422
     end
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
-    if @comment.destroy
-      render json: { success: true, message: "Comment deleted successfully", comment: @comment }, status: :ok
+    post = Post.find_by(id: params[:post_id])
+    comment = post.comments.find_by(id: params[:id])
+    if comment.destroy
+      render json: { success: true, message: "Comment deleted successfully" }, status: :ok
     else
-      render json: { success: false, message: @comment.errors.full_messages, comment: @comment }, status: 422
+      render json: { success: false, message: comment.errors.full_messages}, status: 422
     end
   end
 

db0e45544081da08b766a412b371bcafa91f2900
vic778 <victoiremmanuelbarh@gmail.com>
2023-02-04 23:25:03 +0400

    Set up the host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,3 +70,4 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
+Rails.application.routes.default_url_options[:host] = "http://localhost:3000"

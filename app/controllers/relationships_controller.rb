ac6d4bf836a49e672e508da6d2eddfc08b93f846
vic778 <victoiremmanuelbarh@gmail.com>
2023-02-05 15:58:43 +0400

    Refactor the destroy

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,22 +2,23 @@ class RelationshipsController < PermissionsController
   before_action :authenticate_user!
 
   def create
-    @user = User.find(params[:user_id])
-    @rel = Relationship.new(follower_id: current_user.id, followed_id: params[:user_id])
-    if @rel.save
-      render json: { status: 'SUCCESS', message: 'Followed', data: @rel }, status: :ok
+    user = User.find(params[:user_id])
+    rel = Relationship.new(followed_id: current_user.id, follower_id: user.id)
+    if rel.save
+      render json: { sucess: true, message: "You have followed #{current_user.name} succefuflly", data: rel }, status: :ok
+    else
+      render json: { sucess: false, message: rel.errors }, status: :ok
+    end
+  end
+
+  def destroy
+    followed = Relationship.where(followed_id: current_user.id)
+    rel = followed.find_by(follower_id: params[:user_id])
+    if rel.destroy
+      render json: { status: 'SUCCESS', message: 'Unfollowed', data: rel }, status: :ok
     else
       render json: { status: :error }
     end
   end
 
-  def destroy
-    @user = User.find(params[:user_id])
-    @rel = Relationship.find_by(follower_id: current_user.id, followed_id: params[:user_id])
-    if @rel.destroy
-      render json: { status: 'SUCCESS', message: 'Unfollowed', data: @rel }, status: :ok
-    else
-      render json: { status: :error }
-    end
-  end
 end
